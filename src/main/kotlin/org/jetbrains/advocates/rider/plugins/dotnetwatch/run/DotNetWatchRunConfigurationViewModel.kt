6e768bbac180dcc3afd112dd9d5644f77a17b5d1
Maarten Balliauw <maarten@maartenballiauw.be>
2021-11-04 14:37:28 +0100

    Disable view model while loading

--- src/main/kotlin/org/jetbrains/advocates/rider/plugins/dotnetwatch/run/DotNetWatchRunConfigurationViewModel.kt
+++ src/main/kotlin/org/jetbrains/advocates/rider/plugins/dotnetwatch/run/DotNetWatchRunConfigurationViewModel.kt
@@ -53,6 +53,8 @@ class DotNetWatchRunConfigurationViewModel(
     )
 
     init {
+        disable()
+
         projectSelector.bindTo(
             runnableProjectsModel = runnableProjectsModel,
             lifetime = lifetime,
