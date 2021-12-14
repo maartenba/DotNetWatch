51238bbaad5b8804a5da522dffb8bf0d060459ec
Maarten Balliauw <maarten@maartenballiauw.be>
2021-12-14 11:51:06 +0100

    Drop Qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ plugins {
     id("org.jetbrains.intellij") version "1.1.6"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.0"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.12"
 }
 
 group = properties("pluginGroup")
@@ -53,14 +51,6 @@ intellij {
 changelog {
     version.set(properties("pluginVersion"))
     groups.set(emptyList())
-}
-
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT").toBoolean())
 }
 
 tasks {
